26316b90d68b8eed6c1a02d0a74f446853b39df4
grinil khanna, Minh Phuong and Tammy Dang <minh@>
2017-01-19 10:14:08 -0800

    Add variable for Business Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 //Business Logic
-var Input = 0
+var Input = 0;
+var OneTen = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+var underNinety = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+var underThousand = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var overThousand = ["M", "MM", "MMM"];
+
+function convert () {
+  
+};
+
+
+
 //User Interface
 $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     Input = "#userInput";
+    $("#result").show();
   });
 });
